97c8ddf1a8fb66e7ee8543ec236b2e1ea15afd52
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-13 20:09:57 -0300

    utilizando a classe criada, classe musica

--- Program.cs
+++ Program.cs
@@ -1,5 +1,14 @@
-﻿// nome
-// artista
-// duracao
-// disponivel
+﻿Musica musica1 = new Musica();
+musica1.nome = "Roxanne";
+musica1.artista = "The Police";
+
+Console.WriteLine($"Nome: {musica1.nome}");
+Console.WriteLine($"Artista: {musica1.artista}");
+
+Musica musica2 = new Musica();
+musica2.nome = "Vertigo";
+musica1.artista = "U2";
+
+Console.WriteLine($"Nome: {musica2.nome}");
+Console.WriteLine($"Artista: {musica2.artista}");
 
