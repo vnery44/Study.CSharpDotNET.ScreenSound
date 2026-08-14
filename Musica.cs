7ad9fb1d30f211982be1dbd393e3196e4bcb0e7d
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-14 09:21:59 -0300

    apagamos os metodos de leitura e escritura do disponivel, agora vamos usar o get e set

--- Musica.cs
+++ Musica.cs
@@ -3,24 +3,14 @@
     public string nome;
     public string artista;
     public int duracao;
-    private bool disponivel;
-
-    public void EscreveDisponivel (bool value)
-    {
-        disponivel = value;
-    }
-
-    public bool LeDisponivel() 
-    { 
-        return disponivel; 
-    }
+    public bool Disponivel {get; set;}
 
     public void ExibirFichaTecnica()
     {
         Console.WriteLine($"Nome: {nome}");
         Console.WriteLine($"Artista: {artista}");
         Console.WriteLine($"Duração: {duracao}");
-        if (disponivel)
+        if (Disponivel)
         {
             Console.WriteLine("Disponível no plano.");
         }
--- Program.cs
+++ Program.cs
@@ -2,15 +2,15 @@
 musica1.nome = "Roxanne";
 musica1.artista = "The Police";
 musica1.duracao = 273;
-musica1.EscreveDisponivel(true);
-Console.WriteLine(musica1.LeDisponivel());
+musica1.Disponivel = true;
+Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
 musica2.nome = "Vertigo";
 musica2.artista = "U2";
 musica2.duracao = 367;
-musica2.EscreveDisponivel(false);
-Console.WriteLine(musica2.LeDisponivel());
+musica2.Disponivel= false;
+Console.WriteLine(musica2.Disponivel);
 
 musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
