7939b697b82d8edc97d92c3e21044085a7943fc3
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-14 09:38:19 -0300

    usando apenas o modo get para a propriedade DescricaoResumida e usando o arrow function para atribuir o texto que vai ser exibido

--- Program.cs
+++ Program.cs
@@ -1,14 +1,14 @@
 ﻿Musica musica1 = new Musica();
-musica1.nome = "Roxanne";
-musica1.artista = "The Police";
-musica1.duracao = 273;
+musica1.Nome = "Roxanne";
+musica1.Artista = "The Police";
+musica1.Duracao = 273;
 musica1.Disponivel = true;
 Console.WriteLine(musica1.Disponivel);
 
 Musica musica2 = new Musica();
-musica2.nome = "Vertigo";
-musica2.artista = "U2";
-musica2.duracao = 367;
+musica2.Nome = "Vertigo";
+musica2.Artista = "U2";
+musica2.Duracao = 367;
 musica2.Disponivel= false;
 Console.WriteLine(musica2.Disponivel);
 
