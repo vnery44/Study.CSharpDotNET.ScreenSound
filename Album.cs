ffe4d67efed134cebd7291564dccf6901e74122e
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-15 14:10:32 -0300

    criada a função de exibir as musicas do album, junto a isso a propriedade de DuracaoTotal que soma a propriedade Duração de cada musica

--- Album.cs
+++ Album.cs
@@ -2,9 +2,19 @@
 {
     private List<Musica> musicas = new List<Musica>();
     public string Nome { get; set; }
-    public int DuracaoTotal { get; set; }
+    public int DuracaoTotal => musicas.Sum(mus => mus.Duracao); //Para cada elemento(musica neste caso) que eu receber, me devolva a propriedade Duracao.
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
+    }
+
+    public void ExibirMusicasDoAlbum()
+    {
+        Console.WriteLine($"Lista de músicas do álbum {Nome}:\n");
+        foreach (var musica in musicas)
+        {
+            Console.WriteLine($"Música: {musica.Nome}");
+        }
+        Console.WriteLine($"\nPara ouvir este álbum inteiro você precisa de {DuracaoTotal} segundos");
     }
 }
--- Program.cs
+++ Program.cs
@@ -3,9 +3,13 @@ albumDoQueen.Nome = "A night at the opera";
 
 Musica musica1 = new Musica();
 musica1.Nome = "Love of my life";
+musica1.Duracao = 213;
 
 Musica musica2 = new Musica();
 musica2.Nome = "Bohemian Rhapsody";
+musica2.Duracao = 354;
 
 albumDoQueen.AdicionarMusica(musica1);
 albumDoQueen.AdicionarMusica(musica2);
+
+albumDoQueen.ExibirMusicasDoAlbum();
