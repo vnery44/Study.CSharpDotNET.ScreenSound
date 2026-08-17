3e3b9b7a0ba8edeecac9ff07e3de5a776982c872
Vinicius Nery <vinicius_nery2@hotmail.com>
2026-08-17 16:27:42 -0300

    desafio final, criada as duas classes novas para o desafio, Episodio e Podcast, com lista de convidados em episodio e o relacionamento entre ambas

---
+++ Podcast.cs
@@ -0,0 +1,27 @@
+﻿class Podcast
+{
+    private List<Episodio> episodios = new List<Episodio>();
+    public string Nome { get; }
+    public string Host {  get; }
+    public int TotalEpisodios => episodios.Count;
+    public Podcast(string host, string nome)
+    {
+        Host = host;
+        Nome = nome;
+    }
+
+    public void AdicionarEpisodio(Episodio episodio)
+    {
+        episodios.Add(episodio);
+    }
+
+    public void ExibirDetalhes()
+    {
+        Console.WriteLine($"Podcast {Nome} apresentado por {Host}\n");
+        foreach (Episodio episodio in episodios.OrderBy(e => e.Ordem))
+        {
+            Console.WriteLine(episodio.Resumo);
+        }
+        Console.WriteLine($"\n\nEste podcast possui {TotalEpisodios} episódios.");
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿Banda queen = new Banda("Queen");
+﻿/*
+Banda queen = new Banda("Queen");
 
 Album albumDoQueen = new Album("A night at the opera");
 
@@ -22,3 +23,20 @@ musica1.ExibirFichaTecnica();
 musica2.ExibirFichaTecnica();
 albumDoQueen.ExibirMusicasDoAlbum();
 queen.ExibirDiscografia();
+*/
+
+Episodio ep1 = new(1,"Técnicas de facilitação", 45);
+ep1.AdicionarConvidados("Maria");
+ep1.AdicionarConvidados("Marcelo");
+Console.WriteLine(ep1.Resumo);
+
+Episodio ep2 = new(2,"Técnicas de aprendizado", 67);
+ep2.AdicionarConvidados("Fernando");
+ep2.AdicionarConvidados("Marcos");
+ep2.AdicionarConvidados("Flavia");
+Console.WriteLine(ep2.Resumo);
+
+Podcast podcast1 = new("Podcast especial", "Vinicius");
+podcast1.AdicionarEpisodio(ep1);
+podcast1.AdicionarEpisodio(ep2);
+podcast1.ExibirDetalhes();
